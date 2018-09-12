45017616c6f077572d7c3b08a91f64d59ebafcdf
sathit34 <sathit34@hotmail.co.uk>
2018-09-13 04:11:08 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -26,12 +26,14 @@
 {
       
     $ch = curl_init($url);
+   
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
  
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
      
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+
      
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
      
