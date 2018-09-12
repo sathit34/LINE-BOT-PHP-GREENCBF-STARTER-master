76685515ed4da48a8f21db367db07c7886abe626
sathit34 <sathit34@hotmail.co.uk>
2018-09-13 03:07:46 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,35 +1,47 @@
  <?php
-  
+require(“vendor/autoload.php”);
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
 
-function send_LINE($msg){
- $access_token = 'cmNWCPo+wzwC31jFYBgctzZ8hwNzAv+22WE6hC3agpA1LF/2L/i8qXBTfpw/0uWqCczVZqzU+olQl+W32u6Ib1ItEVPect35SMp8uRFS9c5riG+6yZVT8mordN+Noduqx+QSj+bx0m9GzdCrC9PDxgdB04t89/1O/w1cDnyilFU='; 
+require(“phpMQTT.php”);
 
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
+$mqtt = new phpMQTT(“https://greencbf1.herokuapp.com/”, 1883, “phpMQTT Pub Example”); //เปลี่ยน www.yourmqttserver.com ไปที่ mqtt server ที่เราสมัครไว้นะครับ
 
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/push';
-      $data = [
+$token = “cmNWCPo+wzwC31jFYBgctzZ8hwNzAv+22WE6hC3agpA1LF/2L/i8qXBTfpw/0uWqCczVZqzU+olQl+W32u6Ib1ItEVPect35SMp8uRFS9c5riG+6yZVT8mordN+Noduqx+QSj+bx0m9GzdCrC9PDxgdB04t89/1O/w1cDnyilFU=”; //นำ token ที่มาจาก line developer account ของเรามาใส่ครับ
 
-        'to' => 'U605e53823a22dcdf383a548a2a2a9fd4',
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$httpClient = new CurlHTTPClient($token);
+$bot = new LINEBot($httpClient, [‘channelSecret’ => $token]);
+// webhook
+$jsonStr = file_get_contents(‘php://input’);
+$jsonObj = json_decode($jsonStr);
+print_r($jsonStr);
+foreach ($jsonObj->events as $event) {
+if(‘message’ == $event->type){
+// debug
+//file_put_contents(“message.json”, json_encode($event));
+$text = $event->message->text;
 
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
+if (preg_match(“/สวัสดี/”, $text)) {
+$text = “มีอะไรให้จ่าวิสรับใช้ครับ”;
+}
 
-      echo $result . "\r\n"; 
+if (preg_match(“/เปิดทีวี/”, $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
+if ($mqtt->connect()) {
+$mqtt->publish(“/ESP/REMOTE”,”TV”); // ตัวอย่างคำสั่งเปิดทีวีที่จะส่งไปยัง mqtt server
+$mqtt->close();
+}
+$text = “เปิดทีวีให้แล้วคร้าบบบบ”;
+}
+if (preg_match(“/ปิดทีวี/”, $text) and !preg_match(“/เปิดทีวี/”, $text)) {
+if ($mqtt->connect()) {
+$mqtt->publish(“/ESP/REMOTE”,”TV”);
+$mqtt->close();
+}
+$text = “จ่าปิดทีวีให้แล้วนะครับ!!”;
+}
+$response = $bot->replyText($event->replyToken, $text); // ส่งคำ reply กลับไปยัง line application
+
+}
 }
 
 ?>
